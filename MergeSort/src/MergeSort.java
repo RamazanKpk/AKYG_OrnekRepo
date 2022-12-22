90759559e44f5114a316c8916d9cdc2c7d69caa1
Umut Karadas <81423078+uUMUTt@users.noreply.github.com>
2022-12-22 11:47:03 +0300

    The merge function was added.
    I reviewed your code and realized that there was no merge function in it. After reviewing the code, I recommend that you accept my pull request.

--- MergeSort/src/MergeSort.java
+++ MergeSort/src/MergeSort.java
@@ -32,10 +32,43 @@ public class MergeSort {
         int[] result = new int[dizi.length];
         sol = mergeSort(sol);
         sag = mergeSort(sag);
-        result = merge(sol, sag);
+        result = merge(dizi, sol, sag);
         return result;
     }
+    
+    // Umut Karadas
+    
+    private static void merge(int[] array, int[] left, int[] right) {
+    int leftIndex = 0;
+    int rightIndex = 0;
 
+    int arrayIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        array[arrayIndex] = left[leftIndex];
+        leftIndex++;
+      } else {
+        array[arrayIndex] = right[rightIndex];
+        rightIndex++;
+      }
+      arrayIndex++;
+    }
+
+    while (leftIndex < left.length) {
+      array[arrayIndex] = left[leftIndex];
+      leftIndex++;
+      arrayIndex++;
+    }
+
+    while (rightIndex < right.length) {
+      array[arrayIndex] = right[rightIndex];
+      rightIndex++;
+      arrayIndex++;
+    }
+  }
+
+    // *****************************************************
    
     public static void main(String args[]){
         int [] dizi = {6, 5, 8, 9, 2, 7, 3};
